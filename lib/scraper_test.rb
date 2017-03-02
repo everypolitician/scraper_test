1ddc1c865b9d0964d65b629b9879e46f46e9276f
Chris Mytton <chrismytton@gmail.com>
2017-03-02 17:51:11 +0000

    Initial version of scraper_test library
    This is extracted from the work Oliver did on the Croatia scrapers that
    Tony and I (Chris) then developed into this.

--- lib/scraper_test.rb
+++ lib/scraper_test.rb
@@ -1,5 +1,43 @@
 require 'scraper_test/version'
 
 module ScraperTest
-  # Your code goes here...
+  class RakeTask < ::Rake::TaskLib
+    attr_reader :name
+
+    def initialize(name = :test)
+      @name = name
+    end
+
+    def install_tasks
+      task(name) do
+        require 'minitest/autorun'
+        require 'pry'
+        require 'vcr'
+        require 'webmock'
+
+        Dir['lib/**/*.rb'].each { |f| require File.expand_path(f) }
+
+        VCR.configure do |c|
+          # TODO: Make this configurable
+          c.cassette_library_dir = 'test/cassettes'
+          c.hook_into :webmock
+        end
+
+        describe 'Scraper tests' do
+          it 'should contain the expected data' do
+            # TODO: Make this configurable
+            Dir['test/data/*.yml'].each do |file|
+              yaml_data = YAML.load_file(file).to_h
+              url = yaml_data[:url]
+              class_to_test = Object.const_get(yaml_data[:class])
+              VCR.use_cassette(File.basename(url)) do
+                response = class_to_test.new(response: Scraped::Request.new(url: url).response)
+                yaml_data[:to_h].must_equal response.to_h
+              end
+            end
+          end
+        end
+      end
+    end
+  end
 end
