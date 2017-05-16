e3aee9921513b46c75a1f0823a670bb548e1eca3
Oliver Denman <oliver@mysociety.org>
2017-05-16 10:21:10 +0100

    Move dummy classes to test helper
    A forthcoming commit will add a new test file that references these classes.

--- test/instructions_test.rb
+++ test/instructions_test.rb
@@ -1,21 +1,5 @@
 require 'test_helper'
 
-class DummyClass
-  # This is the class referenced in `./data/valid_instructions.yml`.
-  # For an instructions file to be valid, it must reference a known class.
-  def to_h
-    { id: 34 }
-  end
-end
-
-class DummyMembersList < Scraped::HTML
-  # This is the class referenced in `./data/target_subset_of_members_list_data.yml`.
-  # The instructions file specifies a subset of the data returned by to_h.
-  def to_h
-    { members: [{ id: 1, name: 'Jim' }, { id: 2, name: 'Joe' }] }
-  end
-end
-
 describe ScraperTest::Instructions do
   let(:instructions) { ScraperTest::Instructions.new(file) }
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,3 +3,19 @@ require 'scraped'
 require 'scraper_test'
 
 require 'minitest/autorun'
+
+class DummyClass < Scraped::HTML
+  # This is the class referenced in `./data/valid_instructions.yml`.
+  # For an instructions file to be valid, it must reference a known class.
+  def to_h
+    { id: 34 }
+  end
+end
+
+class DummyMembersList < Scraped::HTML
+  # This is the class referenced in `./data/target_subset_of_members_list_data.yml`.
+  # The instructions file specifies a subset of the data returned by to_h.
+  def to_h
+    { members: [{ id: 1, name: 'Jim' }, { id: 2, name: 'Joe' }] }
+  end
+end
